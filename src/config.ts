9db87223c1392a6ce3851bba86b968c1b48b46d0
KiRura <57588049+KiRura@users.noreply.github.com>
2025-04-12 16:17:49 +0900

    configのデフォルトが15時間になっていたのを15分に修正

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const config = {
 	mentionRoles: ["1149685954366423040"],
 	mentionUsers: [],
 	lastFirstConnectionTimestampTextFile: "./src/data/last.txt",
-	rateLimit: 15 * 60 * 60 * 1000, // milli seconds
+	rateLimit: 15 * 60 * 1000, // milli seconds
 };
